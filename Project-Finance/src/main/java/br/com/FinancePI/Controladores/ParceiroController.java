d39bf04779ab36e496235d2be2cbd39d22059a54
Eduardo Coppi <educoppi00@gmail.com>
2023-11-13 22:26:11 -0300

    ATT tela Parceiros

--- Project-Finance/src/main/java/br/com/FinancePI/Controladores/ParceiroController.java
+++ Project-Finance/src/main/java/br/com/FinancePI/Controladores/ParceiroController.java
@@ -25,6 +25,8 @@ public class ParceiroController implements Serializable {
     @Inject
     private Parceiro parceiro;
 
+    private Parceiro parceiroBusca;
+
 
     public void salvar(){
 
--- Project-Finance/src/main/resources/import.sql
+++ Project-Finance/src/main/resources/import.sql
@@ -1,5 +1,6 @@
 insert into natureza (descricao) values ('imposto'), ('Materia prima'), ('Alimentação'), ('Limpeza');
 
+insert into parceiro (nome, nomefantasia, cnpj, cpf, fornecedor, cliente) values ('Coppi Solutions', 'Coppi', '000000000000100', '00000000000', 1, 1), ('Bravo Solutions', 'Bravo', '003003005000100', null, 1, 0), ('Raphael Alves', 'Raphael SAP', null, '30060050040', 0, 1);
 
 insert into despesa (descricao, dt_vencimento, formapgto, fornecedor, valor, dt_lancamento, categoria_despesa_cod) values ('nescauball', '2023-10-23', 'Boleto', 'Governo', 52000, '2023-10-12', 1), ('Cafe', '2022-10-23', 'Boleto', 'Governo', 100, '2023-10-12', 2), ('nescauball', '2023-10-25', 'Boleto', 'Governo', 10000, '2023-10-12', 3), ('nescauball', '2023-10-10', 'Boleto', 'Governo', 356.45, '2023-10-12', 4);
 
